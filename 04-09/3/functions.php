065f2cff6b711c2ac084b64f8e209dcaf07aff8a
fantomaslt <61119532+fantomaslt@users.noreply.github.com>
2020-04-16 13:55:22 +0300

    aa

--- 04-09/3/functions.php
+++ 04-09/3/functions.php
@@ -4,10 +4,17 @@
 function TextInput($name)
 {
     $text = ucfirst($name);
-    echo "
-            <label for='{$name}'>{$text}</label>
-            <input type='text' name='{$name}' value='' placeholder='Įrašykite tekstą' />
-        ";
+    echo "<label for='{$name}'>{$text}</label>
+          <input type='text' name='{$name}' value='' placeholder='Įrašykite tekstą' />";
 }
 
-
+$new = "";
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (isset($_GET['submit'])) {
+        if (empty($_GET['name'])) {
+            echo 'turite įrašyti pavadinimą';
+        } elseif (isset($_GET['name'])) {
+            $new = TextInput($_GET['name']);
+        }
+    }
+}
