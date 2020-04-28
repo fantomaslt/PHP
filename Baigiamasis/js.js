de367504cde458e1474b5560ccbc9315173f2243
fantomaslt <61119532+fantomaslt@users.noreply.github.com>
2020-04-28 19:47:14 +0300

    baigiamasis darbas

---
+++ Baigiamasis/js.js
@@ -0,0 +1,13 @@
+
+flatpickr("#date-input", {
+    enableTime: false,
+    dateFormat: "Y-m-d H:i",
+})
+flatpickr("#time-input", {
+    enableTime: true,
+        noCalendar: true,
+    dateFormat: "H:i",
+})
+
+
+
